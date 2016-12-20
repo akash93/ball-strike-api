e8d03f9068f5a5c56d80afc046ff277ea0409748
Akash Khan <akashkhan25@gmail.com>
2016-12-20 18:18:40 +0800

    Added user model specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before { @user = FactoryGirl.build :user}
+
+  subject { @user }
+
+  describe 'response' do
+
+    it { should respond_to(:email) }
+    it { should respond_to(:password) }
+    it { should respond_to(:password_confirmation) }
+  end
+
+  describe 'validation' do
+    it {should be_valid}
+  end
 end
