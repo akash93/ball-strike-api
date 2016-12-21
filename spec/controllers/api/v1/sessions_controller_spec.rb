f794c82a8821700af7ba4e50732c9de2c6dcdb2b
Akash Khan <akashkhan25@gmail.com>
2016-12-21 18:37:35 +0800

    Added session controller and  create,destroy actions

---
+++ spec/controllers/api/v1/sessions_controller_spec.rb
@@ -0,0 +1,48 @@
+require 'rails_helper'
+
+RSpec.describe Api::V1::SessionsController, type: :controller do
+
+  describe 'POST #create' do
+    before(:each) do
+      @user = FactoryGirl.create(:user)
+    end
+
+    context 'when credentials are correct' do
+      before(:each) do
+        credentials = {email: @user.email, password: 'password'}
+        post :create, params: { session: credentials }
+      end
+
+      it 'returns the user corresponding to given credentials' do
+        @user.reload
+        expect(json_response[:email]).to eql @user.email
+      end
+
+      it { should respond_with 200 }
+    end
+
+    context 'when credentials are incorrect' do
+      before(:each) do
+        credentials = { email: @user.email, password: 'wrongpassword' }
+        post :create, params: { session: credentials }
+      end
+
+      it 'should return json with errors' do
+        expect(json_response[:errors]).to eql 'Invalid credentials'
+      end
+
+      it {should respond_with 403}
+    end
+  end
+
+  describe 'DELETE #destroy' do
+    before(:each) do
+      @user = FactoryGirl.create :user
+      sign_in @user
+      delete :destroy, params: {id: @user.auth_token}
+    end
+
+    it {should respond_with 204}
+  end
+
+end
