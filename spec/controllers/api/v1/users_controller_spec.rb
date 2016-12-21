baab165700ace8e7892f8ef2191d9157081dea62
Akash Khan <akashkhan25@gmail.com>
2016-12-21 11:09:31 +0800

    Added show action for user

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -4,4 +4,18 @@ RSpec.describe Api::V1::UsersController, type: :controller do
   before(:each) do
     request.headers['Accept'] = 'application/vnd.ballstrike.v1'
   end
+
+  describe 'GET #show' do
+    before(:each) do
+      @user = FactoryGirl.create :user
+      get :show, params: { id: @user.id }, format: :json
+    end
+
+    it 'returns information of user' do
+      user_response = JSON.parse(response.body, symbolize_names: true)
+      expect(user_response[:email]).to eql @user.email
+    end
+
+    it {should respond_with 200}
+  end
 end
