481753b430d4d17a50a396cf05404b6c0cb2cb63
Akash Khan <akashkhan25@gmail.com>
2016-12-21 09:52:25 +0800

    Added validation tests for user attributes

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,5 +14,15 @@ RSpec.describe User, type: :model do
 
   describe 'validation' do
     it {should be_valid}
+    it {should validate_presence_of(:email)}
+    it {should validate_uniqueness_of(:email).ignoring_case_sensitivity}
+    it {should validate_confirmation_of(:password)}
+    it {should allow_value('user@example.com').for(:email)}
+  
+  end
+
+  describe 'when email is absent' do
+    before { @user.email = '' }
+    it {should_not be_valid}
   end
 end
