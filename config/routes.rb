123d108e3fff2e862cd977b21aab502b0adbf213
Akash Khan <akashkhan25@gmail.com>
2016-12-21 13:55:17 +0800

    Modified routes to fix clash with devise routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ require 'api_constraints'
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  namespace :api, defaults: {format: :json}, path: '/' do
+  namespace :api, defaults: {format: :json} do
     scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
       resources :users, only: [:show, :create]
     end
