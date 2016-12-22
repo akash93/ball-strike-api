96b4167fa6ffb6eb0c6676294f705eab4850f197
Akash Khan <akashkhan25@gmail.com>
2016-12-22 18:11:36 +0800

    Added create action for gamelevel

--- app/controllers/api/v1/gamelevels_controller.rb
+++ app/controllers/api/v1/gamelevels_controller.rb
@@ -1,8 +1,23 @@
 class Api::V1::GamelevelsController < ApplicationController
   respond_to :json
-  before_action :authenticate_with_token!, only: [:index]
+  before_action :authenticate_with_token!, only: [:index, :create]
   def index
     levels = Gamelevel.all
     render json: levels
   end
+
+  def create
+    gamelevel = Gamelevel.new(level_params)
+    if gamelevel.save
+      render json: gamelevel, status: 201
+    else
+      render json: {errors: gamelevel.errors}, status: 422
+    end
+  end
+
+  private
+    
+    def level_params
+      params.require(:gamelevel).permit(:num_enemies, :num_balls, :num_hits_per_enemy)
+    end
 end
--- spec/controllers/api/v1/gamelevels_controller_spec.rb
+++ spec/controllers/api/v1/gamelevels_controller_spec.rb
@@ -17,4 +17,24 @@ RSpec.describe Api::V1::GamelevelsController, type: :controller do
     it { should respond_with 200 }
 
   end
+
+  describe 'POST #create' do
+    before do
+      @user = FactoryGirl.create :user
+      api_authorization_header @user.auth_token
+    end
+    context 'when level is successfully created' do
+      before(:each) do
+        @level_attributes = FactoryGirl.attributes_for :gamelevel
+        post :create, params: { gamelevel: @level_attributes }, format: :json
+      end
+
+      it 'returns the json representation of level created' do
+        expect(json_response[:num_enemies]).to eql @level_attributes[:num_enemies]
+      end
+
+      it { should respond_with 201 }
+    end
+
+  end
 end
