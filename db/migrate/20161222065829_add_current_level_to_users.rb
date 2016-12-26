4b0464f0592de1c3814c94341dd92eadb96992b2
Akash Khan <akashkhan25@gmail.com>
2016-12-26 10:57:43 +0800

    Fixing build errors due to migration issues

--- db/migrate/20161222065829_add_current_level_to_users.rb
+++ db/migrate/20161222065829_add_current_level_to_users.rb
@@ -1,6 +1,7 @@
 class AddCurrentLevelToUsers < ActiveRecord::Migration[5.0]
   def change
     add_column :users, :current_level_id, :integer
+
+    add_index :users, :current_level_id
   end
-  add_index :users, :current_level_id
 end
