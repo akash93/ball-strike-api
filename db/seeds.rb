f59573d4a22bb220e4bfd4e0db239a1da2799fa7
Akash Khan <akashkhan25@gmail.com>
2016-12-28 10:16:07 +0800

    Added test user for seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create(email: 'akash@gmail.com', password: 'password');
-User.create(email: 'akashkhan25@gmail.com', password: 'password');
-
+User.create(email: 'user@example.com', password: 'password')
 
 10.times do |i|
   Gamelevel.create(num_balls:1, num_enemies: 2, num_hits_per_enemy: 5*(i+1))
